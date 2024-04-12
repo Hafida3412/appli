8570197d10c3f2019285506d5bd065a1a0bd4557
Hafida 3412 <lounsy81@gmail.com>
2024-04-12 14:51:36 +0200

    appli 1ère partie terminée

--- recap.php
+++ recap.php
@@ -42,6 +42,12 @@ session_start();
 
 var_dump($_SESSION);*/
 
+               
+    /*$product['name'] =  "pomme";
+    $product ['price'] = "2.5";
+    $product['qtt'] = 10;
+    $product['total'] = 2.5*10;*/
+
 if(!isset($_SESSION['products']) || empty($_SESSION['products'])){ //Soit la clé "products" du tableau de session $_SESSION n'existe pas : !isset()
                                                                   
     echo "<p>Aucun produit en session...</p>";                      
@@ -54,11 +60,51 @@ echo "<table>",                                                                 
 "<th>Prix</th>",
 "<th>Quantité</th>",
 "<th>Total</th>",
-"<tr>",
-"<thead>",
-"<tbody>",
-
+"</tr>",
+"</thead>",
+"<tbody>";
+$totalGeneral = 0;
+foreach($_SESSION['products'] as $index => $product){       //$index aura pour valeur l'index du tableau $_SESSION['products'] parcouru
+ echo "<tr>",                                                          //$product contiendra le produit, sous forme de tableau, tel que l'a créé 
+        "<td>".$index."</td>",                                                   //et stocké en session le fichier traitement.php
+        "<td>".$product['name']."</td>",
+        "<td>".number_format($product['price'], 2, ",","&nbsp;")."&nbsp;€</td>",
+        "<td>".$product['qtt']."</td>",
+        "<td>".number_format($product['total'], 2, ",","&nbsp;")."&nbsp;€</td>",
+        "</tr>";
+$totalGeneral+= $product['total'];  /* grâce à l'opérateur combiné +=, on ajoute le total du produit 
+parcouru à la valeur de $totalGeneral, qui augmente d'autant pour chaque produit*/
 }
+
+/*La fonction PHP number_format permet de modifier l'affichage d'une valeur numérique, en précisant:
+    number_format(
+    variable à modifier, 
+    nombre de décimales souhaité, 
+    caractère séparateur décimal,
+    caractère séparateur de milliers5
+    );*/
+
+
+/*Une fois la boucle terminée, nous affichons une dernière ligne avant de refermer notre 
+tableau. Cette ligne contient deux cellules : une cellule fusionnée de 4 cellules (colspan=4) 
+pour l'intitulé, et une cellule affichant le contenu formaté de $totalGeneral avec 
+number_format()*/
+echo "<tr>",
+      "<td colspan=4>Total général: </td>",
+      "<td><strong>".number_format($totalGeneral, 2, ",","&nbsp;")."&nbsp;€<strong></td>",
+      "</tr>",
+
+"</tbody>",
+     "</table>";
+}
+
+//TEST POUR VERIFIER SI CA FONCTIONNE:
+$products = [
+    ['name' => 'banane', 'price' => 1.25, 'qtt' => 3, 'total' => 3.75],
+    ['name' => 'pomme', 'price' => 2.50, 'qtt' => 10, 'total' => 25.00]
+];
+
+$_SESSION['products'] = $products;
 
 ?>
     
